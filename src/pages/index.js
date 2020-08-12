8ca99bc994868c407ee39318b468a269f84954a9
Cully Wakelin <cullywakelin@gmail.com>
2020-08-12 15:40:17 -0600

    changing docs landing page to get-started

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,7 @@ function Feature({ imageUrl, title, description }) {
 function Home() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
-  return <Redirect to='/docs/overview' />;
+  return <Redirect to='/docs/get-started' />;
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
