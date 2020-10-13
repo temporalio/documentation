0472d28c9dfd0be5b4fab13e4d35a1e790ef53ac
Cully Wakelin <cullywakelin@gmail.com>
2020-10-13 08:18:32 -0600

    convert screen shots to autoplay loop recording

--- src/components/responsive-player/ResponsivePlayer.js
+++ src/components/responsive-player/ResponsivePlayer.js
@@ -3,12 +3,14 @@ import ReactPlayer from 'react-player';
 
 import styles from './ResponsivePlayer.module.css';
 
-function ResponsivePlayer({ url }) {
+function ResponsivePlayer({ url, loop, playing }) {
   return (
     <div className={styles.playerWrapper}>
       <ReactPlayer
         className={styles.player}
         url={url}
+        loop={loop}
+        playing={playing}
         width="100%"
         height="100%"
       />
