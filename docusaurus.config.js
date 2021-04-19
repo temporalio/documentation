9a0752e6ab5bbd467d839f21630c174aa0a9c93c
swyx <shawnthe1@gmail.com>
2021-04-19 23:39:45 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -187,7 +187,7 @@ module.exports = {
       indexName: 'temporal',
       // contextualSearch: true, // Optional, If you different version of docs etc (v1 and v2) doesn't display dup results
       // appId: 'app-id', // Optional, if you run the DocSearch crawler on your own
-      searchOptions: { // used to be called algoliaOptions
+      searchParameters: { // used to be called algoliaOptions https://docusaurus.io/docs/search#using-algolia-docsearch
         facetFilters: null,
         facets: null
       }
