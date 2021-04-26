421fef129292b2a66ddd40c4c94b4cd81af7da14
sw-yx <shawnthe1@gmail.com>
2021-04-27 06:14:44 +0800

    make responsive

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -152,10 +152,12 @@ module.exports = {
         <a href="https://twitter.com/temporalio" aria-label="Twitter"><svg class="footer__svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg></a>
         <a href="https://www.youtube.com/channel/UCGovZyy8OfFPNlNV0i1fI1g" aria-label="YouTube"><svg class="footer__svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/></svg></a>
       </div>
-      <div>Copyright © ${new Date().getFullYear()} Temporal Technologies Inc.</div>
+      <div class="footer__copyright"><span class="footer__block">Copyright © ${new Date().getFullYear()}</span> Temporal Technologies Inc.</div>
       <div>
-        <a class="footer__link-item" href="/about">About The Docs</a> |
-        <a class="footer__link-item" href="/privacy-policy">Privacy Policy</a> |  
+        <a class="footer__link-item" href="/about">About The Docs</a>
+        <span class="footer__separators"> | </span>
+        <a class="footer__link-item" href="/privacy-policy">Privacy Policy</a>
+        <span class="footer__separators"> | </span>
         <a class="footer__link-item" href="/https://docs.temporal.io/pdf/temporal-tos-2021-01-19.pdf">Terms of Service</a> 
       </div>
       `,
