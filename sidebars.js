b8a855142f5e25d1174b7614c34808c73af5c64b
Tihomir Surdilovic <tihomir@temporal.io>
2021-04-29 16:30:12 -0400

    Adding Continue As New doc page to Java docs
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>

--- sidebars.js
+++ sidebars.js
@@ -181,6 +181,7 @@ module.exports = {
         'java/implementing-workflows',
         'java/starting-workflow-executions',
         'java/versioning',
+        'java/continue-as-new',
         'java/side-effect',
         'java/testing-and-debugging',
         'java/distributed-cron',
