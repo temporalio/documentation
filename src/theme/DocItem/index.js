b515713eb61814259c0b4b9309741f77ba4a0d84
Arijit Kundu <kunduarijit55@gmail.com>
2021-07-06 05:03:24 +0530

    chore: Updated DocItem swizzled component as per beta.3

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -6,30 +6,25 @@
  */
 import React from "react";
 import DocPaginator from "@theme/DocPaginator";
-import DocVersionSuggestions from "@theme/DocVersionSuggestions";
+import DocVersionBanner from "@theme/DocVersionBanner";
 import Seo from "@theme/Seo";
 import LastUpdated from "@theme/LastUpdated";
 import TOC from "@theme/TOC";
 import EditThisPage from "@theme/EditThisPage";
+import {MainHeading} from "@theme/Heading";
 import clsx from "clsx";
 import styles from "./styles.module.css";
-import {
-  useActivePlugin,
-  useVersions,
-  useActiveVersion,
-} from "@theme/hooks/useDocs";
+import {useActivePlugin, useVersions} from "@theme/hooks/useDocs";
 
 function DocItem(props) {
-  const {content: DocContent} = props;
+  const {content: DocContent, versionMetadata} = props;
+  const {metadata, frontMatter} = DocContent;
   const {
-    metadata,
-    frontMatter: {
-      image,
-      keywords,
-      hide_title: hideTitle,
-      hide_table_of_contents: hideTableOfContents,
-    },
-  } = DocContent;
+    image,
+    keywords,
+    hide_title: hideTitle,
+    hide_table_of_contents: hideTableOfContents,
+  } = frontMatter;
   const {
     description,
     title,
@@ -41,12 +36,16 @@ function DocItem(props) {
   const {pluginId} = useActivePlugin({
     failfast: true,
   });
-  const versions = useVersions(pluginId);
-  const version = useActiveVersion(pluginId); // If site is not versioned or only one version is included
+  const versions = useVersions(pluginId); // If site is not versioned or only one version is included
   // we don't show the version badge
   // See https://github.com/facebook/docusaurus/issues/3362
 
-  const showVersionBadge = versions.length > 1;
+  const showVersionBadge = versions.length > 1; // We only add a title if:
+  // - user asks to hide it with frontmatter
+  // - the markdown content does not already contain a top-level h1 heading
+
+  const shouldAddTitle =
+    !hideTitle && typeof DocContent.contentTitle === "undefined";
   return (
     <>
       <Seo
@@ -64,22 +63,23 @@ function DocItem(props) {
             [styles.docItemCol]: !hideTableOfContents,
           })}
         >
-          <DocVersionSuggestions />
+          <DocVersionBanner versionMetadata={versionMetadata} />
           <div className={styles.docItemContainer}>
             <article>
               {showVersionBadge && (
                 <div>
                   <span className="badge badge--secondary">
-                    Version: {version.label}
+                    Version: {versionMetadata.label}
                   </span>
                 </div>
               )}
-              {!hideTitle && (
-                <header>
-                  <h1 className={styles.docTitle}>{title}</h1>
-                </header>
-              )}
               <div className="markdown">
+                {/*
+                  Title can be declared inside md content or declared through frontmatter and added manually
+                  To make both cases consistent, the added title is added under the same div.markdown block
+                  See https://github.com/facebook/docusaurus/pull/4882#issuecomment-853021120
+                  */}
+                {shouldAddTitle && <MainHeading>{title}</MainHeading>}
                 <DocContent />
               </div>
             </article>
