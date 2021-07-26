74c90a18b43c3c99c13624669956b60d356e7cba
swyx <shawnthe1@gmail.com>
2021-07-26 16:13:43 -0700

    add new TOS (#542)
    Co-authored-by: sw-yx <sw-yx@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -182,7 +182,7 @@ module.exports = {
         <span class="footer__separators"> | </span>
         <a class="footer__link-item" href="/privacy-policy">Privacy Policy</a>
         <span class="footer__separators"> | </span>
-        <a class="footer__link-item" href="https://docs.temporal.io/pdf/temporal-tos-2021-01-19.pdf">Terms of Service</a> 
+        <a class="footer__link-item" href="https://docs.temporal.io/pdf/temporal-tos-2021-07-24.pdf">Terms of Service</a> 
       </div>
       `,
     },
@@ -230,8 +230,7 @@ module.exports = {
           routeBasePath: "blog",
           path: "blog",
           postsPerPage: 10,
-          editUrl:
-            "https://github.com/temporalio/documentation/blob/master",
+          editUrl: "https://github.com/temporalio/documentation/blob/master",
           blogTitle: "Temporal Blog",
           showReadingTime: true, // Show estimated reading time for the blog post.
           feedOptions: {
