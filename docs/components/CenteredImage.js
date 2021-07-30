4321a778c95b9d52b121f9c33f30678d95345817
Cully Wakelin <cullywakelin@gmail.com>
2021-07-30 11:57:54 -0600

    exchanged image for svg, updated image component to support a legend

--- docs/components/CenteredImage.js
+++ docs/components/CenteredImage.js
@@ -1,6 +1,17 @@
 import React from "react";
 
-export default function CenteredImage({imagePath, imageSize, title}) {
+export default function CenteredImage({imagePath, imageSize, title, legendstring}) {
+  const legendList = [];
+  if (legendstring != undefined) {
+    const items = legendstring.split("|");
+    for (const item of items) {
+      const parts = item.split("?");
+      legendList.push({
+        symbol: parts[0],
+        description: parts[1],
+      });
+    }
+  }
   let imageClass = "";
   switch (imageSize) {
     case "50":
@@ -20,6 +31,15 @@ export default function CenteredImage({imagePath, imageSize, title}) {
       <div className={"docs-centered-image-wrapper"}>
         <img className={imageClass} src={imagePath} alt={title} />
       </div>
+      {legendList.length > 0 && (
+        <div className={"docs-image-legend-wrapper"}>
+          <ul className={"docs-image-legend-list"}>
+          {legendList.map(({symbol, description}) => (
+            <li><span className={"docs-image-legend-symbol"}>{symbol}</span> = {description}</li>
+          ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
