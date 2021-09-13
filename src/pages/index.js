217557d900ba69f5caea399a4d3895543c718e6f
m-abdelwahab <hey@mahmoud.codes>
2021-09-13 14:21:14 +0200

    Homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,290 +1,36 @@
 import React from "react";
-import clsx from "clsx";
 import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import useBaseUrl from "@docusaurus/useBaseUrl";
-import styles from "./styles.module.css";
-// import {ResponsivePlayer} from "../components";
-
-const features = [
-  {
-    title: <>Server management</>,
-    imageUrl: "img/server.png",
-    description: (
-      <>Deploy and manage your own instance of the Temporal Server.</>
-    ),
-    goto: "/docs/server/introduction",
-  },
-  {
-    title: <>Application development</>,
-    imageUrl: "img/layers.png",
-    description: (
-      <>Find SDK tutorials and API references in the language of your choice.</>
-    ),
-    goto: "/application-development",
-  },
-  {
-    title: <>System tools</>,
-    imageUrl: "img/settings.png",
-    description: <>Visualize and debug using the Temporal Web UI and CLI.</>,
-    goto: "/docs/system-tools/introduction",
-  },
-];
-
-const sdks = [
-  {
-    title: "go SDK",
-    imgUrl: "img/go-lang.svg",
-    goto: "/docs/go/run-your-first-app-tutorial",
-  },
-  {
-    title: "java SDK",
-    imgUrl: "img/java.svg",
-    goto: "/docs/java/run-your-first-app-tutorial",
-  },
-  {
-    title: "php SDK",
-    imgUrl: "img/php.svg",
-    goto: "/docs/php/introduction",
-  },
-  {
-    title: "node SDK",
-    imgUrl: "img/nodejs.svg",
-    goto: "/docs/node/hello-world",
-  },
-];
-
-function Feature({imageUrl, title, description, goto}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={clsx("col col--4", styles.featuresMarginBottom)}>
-      <h3>{title}</h3>
-      <Link to={useBaseUrl(goto)}>
-        {imgUrl && (
-          <div className={clsx("text--center", styles.featureImageContainer)}>
-            <img className={styles.featureImage} src={imgUrl} alt={title} />
-          </div>
-        )}
-      </Link>
-      <p>{description}</p>
-      <div className={styles.buttons}>
-        <Link
-          className={clsx(
-            "button button--outline button--primary button--md",
-            styles.getStarted
-          )}
-          to={useBaseUrl(goto)}
-        >
-          Get Started
-        </Link>
-      </div>
-      <div className={styles.spacer}></div>
-    </div>
-  );
-}
-
-function SelectTutorial() {
-  const [clicked, setClicked] = React.useState(false);
-  return (
-    <div>
-      <div>
-        <div className={styles.buttons}>
-          <button
-            onClick={() => setClicked(!clicked)}
-            className={clsx(
-              "button button--outline button--secondary button--lg",
-              styles.getStarted
-            )}
-          >
-            Run my first app! →
-          </button>
-        </div>
-        {clicked && sdks.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className={clsx("row", styles.justifyCenter)}>
-                {sdks.map(({title, imgUrl, goto}, idx) => (
-                  <div className="col col--2">
-                    <Link to={useBaseUrl(goto)}>
-                      {imgUrl && (
-                        <div
-                          className={clsx(
-                            "text--center",
-                            styles.featureImageContainer
-                          )}
-                        >
-                          <img
-                            className={styles.featureImage}
-                            src={imgUrl}
-                            alt={title}
-                            aria-label={title}
-                          />
-                        </div>
-                      )}
-                    </Link>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
-      </div>
-    </div>
-  );
-}
-
-function TemporalCloud() {
-  return (
-    <div className={styles.formFeature}>
-      <h2 id="cloud">☁️ Temporal Cloud</h2>
-      <p>
-        Temporal Cloud is a fully managed cloud offering of the open-source
-        Server. What are we offering?
-      </p>
-      <p>
-        <ul>
-          <li>
-            <strong>Updates</strong>: Automatic updates with latest releases.
-          </li>
-          <li>
-            <strong>Experience</strong>: Our team has years of experience
-            operating the technology in production environments.
-          </li>
-          <li>
-            <strong>Scale</strong>: Our design partners are multibillion dollar
-            publicly listed companies.
-          </li>
-          <li>
-            <strong>Dependencies</strong>: No more managing dependencies like
-            Elasticsearch, Cassandra or MySQL.
-          </li>
-          <li>
-            <strong>Prioritization</strong>: Dedicated channels for support and
-            product feedback.
-          </li>
-        </ul>
-      </p>
-      <p>
-        Temporal Cloud is working with early design partners today. Sign up to
-        the waitlist and receive updates!
-      </p>
-
-      <Link
-        className={clsx(
-          "button button--outline button--primary button--md",
-          styles.getStarted
-        )}
-        to={
-          "https://us17.list-manage.com/survey?u=2334a0f23e55fd1840613755d&id=f1895b6f4a"
-        }
-      >
-        Book your place
-      </Link>
-    </div>
-  );
-}
+import {Community, SDKs, TemporalCloud, Intro, Related} from "../components";
 
 export default function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
   return (
-    <Layout title={`${siteConfig.title}`} description={`${siteConfig.tagline}`}>
-      <header className={clsx("hero hero--primary", styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <SelectTutorial />
-        </div>
-      </header>
-      <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
-      </main>
-      <div
-        className={clsx("hero hero--secondary container", styles.heroSecondRow)}
+    <div className="main">
+      <Layout
+        className="max-w-screen-lg w-full px-8 my-2 mx-auto shadow-none"
+        title={`${siteConfig.title}`}
+        description={`${siteConfig.tagline}`}
       >
-        <div className="container">
-          <h2>🍎 Core concepts</h2>
-          <p>Review some of Temporal's core concepts and building blocks.</p>
-          <ul>
-            <li>
-              <Link to={useBaseUrl("/docs/concepts/introduction")}>
-                {" "}
-                Introduction{" "}
-              </Link>
-            </li>
-            <li>
-              <Link to={useBaseUrl("/docs/concepts/workflows")}>
-                {" "}
-                Workflows{" "}
-              </Link>
-            </li>
-            <li>
-              <Link to={useBaseUrl("/docs/concepts/activities")}>
-                {" "}
-                Activities{" "}
-              </Link>
-            </li>
-            <li>
-              <Link to={useBaseUrl("/docs/concepts/workers")}> Workers </Link>
-            </li>
-            <li>
-              More:{" "}
-              <Link to={useBaseUrl("/docs/concepts/task-queues")}>
-                {" "}
-                Task Queues
-              </Link>
-              , <Link to={useBaseUrl("/docs/concepts/signals")}>
-                {" "}
-                Signals
-              </Link>,{" "}
-              <Link to={useBaseUrl("/docs/concepts/queries")}>Queries </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="container">
-          <h2>🔥 Additional Resources</h2>
-          <p>
-            Everything else to help you learn Temporal and pitch it internally.
-          </p>
-          <ul>
-            <li>
-              <Link to={useBaseUrl("/docs/external-resources")}>
-                {" "}
-                External Resources{" "}
-              </Link>{" "}
-              - Why Temporal, Tutorials, How It Works
-            </li>
-            <li>
-              <Link to={useBaseUrl("/blog/tags/case-study")}>
-                {" "}
-                Case Studies{" "}
-              </Link>{" "}
-              - Big names use Temporal for big things!
-            </li>
-            <li>
-              <Link to={useBaseUrl("/docs/cadence-to-temporal")}>
-                {" "}
-                Migrate from Cadence{" "}
-              </Link>{" "}
-              - There are key differences, but it's easy!
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="container">
-        <TemporalCloud />
-      </div>
-    </Layout>
+        <main className="max-w-screen-lg p-6 md:p-10 mx-auto">
+          <header className="my-10">
+            <h1 className="text-4xl md:text-5xl tracking-wide mb-4">
+              {siteConfig.title}
+            </h1>
+            <p className="text-xl max-w-2xl">
+              Temporal is the open source runtime for orchestrating highly
+              reliable, mission-critical applications at any scale.
+            </p>
+          </header>
+
+          <Intro />
+          <SDKs />
+          <Related />
+          <Community />
+          <TemporalCloud />
+        </main>
+      </Layout>
+    </div>
   );
 }
