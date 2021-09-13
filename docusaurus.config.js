d7533e3bfa130a51691661f133b83ece43ac4564
m-abdelwahab <hey@mahmoud.codes>
2021-09-13 14:15:43 +0200

    Update navbar icons and footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,15 +16,28 @@ module.exports = {
   plugins: ["docusaurus-tailwindcss-loader"],
   themeConfig: {
     colorMode: {
-      defaultMode: "light",
+      defaultMode: "dark",
       disableSwitch: false,
+      switchConfig: {
+        darkIcon: "🌙",
+        darkIconStyle: {
+          content: `url(/img/moon.svg)`,
+          transform: "scale(2)",
+          margin: "0 0.2rem",
+        },
+        lightIcon: "\u{1F602}",
+        lightIconStyle: {
+          content: `url(/img/sun.svg)`,
+          transform: "scale(2)",
+        },
+      },
     },
     prism: {
       theme: require("prism-react-renderer/themes/nightOwlLight"),
       darkTheme: require("prism-react-renderer/themes/dracula"),
       additionalLanguages: ["java", "ruby", "php"],
     },
-    hideableSidebar: true,
+    // hideableSidebar: true,
     navbar: {
       hideOnScroll: true,
       logo: {
@@ -48,15 +61,15 @@ module.exports = {
               label: "Server",
             },
             {
+              to: "/docs/system-tools/introduction",
+              activeBasePath: "/docs/system-tools/",
+              label: "System Tools",
+            },
+            {
               to: "/application-development",
               activeBaseRegex:
                 "(/application-development)|(/docs/(go|java|php|node))",
               label: "SDKs",
-            },
-            {
-              to: "/docs/system-tools/introduction",
-              activeBasePath: "/docs/system-tools/",
-              label: "System Tools",
             },
           ],
         },
@@ -98,14 +111,10 @@ module.exports = {
       ],
     },
     footer: {
-      style: "dark",
       links: [
         {
+          title: "SDKs",
           items: [
-            {
-              label: "All SDKs",
-              to: "/application-development",
-            },
             {
               label: "Go SDK",
               to: "/docs/go/introduction",
@@ -119,12 +128,13 @@ module.exports = {
               to: "/docs/php/introduction",
             },
             {
-              label: "Node SDK (alpha)",
+              label: "TypeScript SDK (alpha)",
               to: "/docs/node/introduction",
             },
           ],
         },
         {
+          title: "Resources",
           items: [
             {
               label: "Core concepts",
@@ -142,9 +152,18 @@ module.exports = {
               label: "Glossary",
               to: "/docs/glossary",
             },
+            {
+              label: "Migrate from Cadence",
+              to: "/docs/cadence-to-temporal",
+            },
+            {
+              label: "External resources",
+              to: "/docs/external-resources",
+            },
           ],
         },
         {
+          title: "Community",
           items: [
             {
               label: "Support Forum",
@@ -155,16 +174,13 @@ module.exports = {
               href: "https://join.slack.com/t/temporalio/shared_invite/zt-onhti57l-J0bl~Tr7MqSUnIc1upjRkw",
             },
             {
-              label: "Temporal Careers",
-              href: "https://temporal.io/careers",
-            },
-            {
               label: "Open Office Hours",
               href: "https://lu.ma/temporal",
             },
           ],
         },
         {
+          title: "Company",
           items: [
             {
               label: "Case Studies",
@@ -175,14 +191,14 @@ module.exports = {
               href: "/blog",
             },
             {
-              label: "External resources",
-              to: "/docs/external-resources",
+              label: "Temporal Careers",
+              href: "https://temporal.io/careers",
             },
           ],
         },
       ],
       copyright: `
-      <div>
+      <div className="footer__icons">
         <a href="https://github.com/temporalio/temporal" aria-label="GitHub"><svg class="footer__svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>
         <a href="https://twitter.com/temporalio" aria-label="Twitter"><svg class="footer__svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg></a>
         <a href="https://www.youtube.com/channel/UCGovZyy8OfFPNlNV0i1fI1g" aria-label="YouTube"><svg class="footer__svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/></svg></a>
