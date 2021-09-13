40c61ac37346f093b44ec34821fa7bc9d41055f5
m-abdelwahab <hey@mahmoud.codes>
2021-09-13 14:20:11 +0200

    refactor: single import for components

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,7 @@
 export {default as ResponsivePlayer} from "./responsive-player/ResponsivePlayer";
+export {Button} from "./shared/Button";
+export {SDKs} from "./SDKs";
+export {TemporalCloud} from "./TemporalCloud";
+export {Community} from "./Community";
+export {Intro} from "./Intro";
+export {Related} from "./Related";
