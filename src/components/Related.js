2dea3e8b684dd78ef985f2a5dc7bf790b11b731b
m-abdelwahab <hey@mahmoud.codes>
2021-09-13 14:18:56 +0200

    add component for mentioning extra links

---
+++ src/components/Related.js
@@ -0,0 +1,83 @@
+import React from "react";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import Link from "@docusaurus/Link";
+
+export const Related = () => {
+  return (
+    <section className="my-12">
+      <h2 className="text-3xl md:text-4xl">Related resources</h2>
+      <ul className="mt-5  flex flex-col space-y-3 text-lg">
+        <li className="flex items-center space-x-2 hover:underline">
+          <svg
+            className="w-4 h-4"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+            <path
+              fillRule="evenodd"
+              d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <Link to={useBaseUrl("/docs/external-resources#how-temporal-works")}>
+            How Temporal works
+          </Link>
+        </li>
+        <li className="flex items-center space-x-2">
+          <svg
+            className="w-4 h-4"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+            <path
+              fillRule="evenodd"
+              d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <Link
+            to={useBaseUrl(
+              "/docs/external-resources#why-workflow-orchestration"
+            )}
+          >
+            Why workflow orchestration
+          </Link>
+        </li>
+        <li className="flex items-center space-x-2 hover:underline">
+          <svg
+            className="w-4 h-4"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+            <path
+              fillRule="evenodd"
+              d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <Link to={useBaseUrl("/docs/external-resources#temporal-users")}>
+            Temporal users
+          </Link>
+        </li>
+      </ul>
+    </section>
+  );
+};
