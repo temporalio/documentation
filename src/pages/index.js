bbebb21358cbd6ef8c2937b42f4b2b971607f76e
m-abdelwahab <hey@mahmoud.codes>
2021-10-08 16:28:47 +0200

    add home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import {Community, SDKs, TemporalCloud, Intro, Related} from "../components";
+import {
+  Community,
+  TemporalCloud,
+  Intro,
+  Resources,
+  Newsletter,
+} from "../components";
 
 export default function Home() {
   const context = useDocusaurusContext();
@@ -14,20 +20,10 @@ export default function Home() {
         description={`${siteConfig.tagline}`}
       >
         <main className="max-w-screen-lg p-6 md:p-10 mx-auto">
-          <header className="my-10">
-            <h1 className="text-4xl md:text-5xl tracking-wide mb-4">
-              {siteConfig.title}
-            </h1>
-            <p className="text-xl max-w-2xl">
-              Temporal is the open source runtime for orchestrating highly
-              reliable, mission-critical applications at any scale.
-            </p>
-          </header>
-
           <Intro />
-          <SDKs />
-          <Related />
+          <Resources />
           <Community />
+          <Newsletter />
           <TemporalCloud />
         </main>
       </Layout>
