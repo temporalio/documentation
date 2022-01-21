774cb97268dd3fa1f7ea4eae0aa0eb4c6b4cfc8f
swyx <shawnthe1@gmail.com>
2022-01-22 00:00:00 +0800

    Update src/pages/security.js
    Co-authored-by: Dail Magee Jr <7518128+djmagee@users.noreply.github.com>

--- src/pages/security.js
+++ src/pages/security.js
@@ -66,7 +66,7 @@ export default function Security() {
             <div className={clsx("col col--4", styles.justifyLeft)}>
               <p>
                 Temporal Technologies Inc was issued a clean SOC2 Type 2 report
-                on July 31, 2021 from{" "}
+                on July 31, 2021, from{" "}
                 <a href="https://www.schneiderdowns.com/">Schneider Downs</a>
               </p>
               <div className={styles.justifyCenter}>
