246cd178fb54f4b8fdbfcc7af1146ec916b09afa
flossypurse <flossypurse@users.noreply.github.com>
2022-04-25 19:29:05 +0000

    Prettified Code!

--- src/pages/leadership.js
+++ src/pages/leadership.js
@@ -14,55 +14,69 @@ export default function Security() {
       <div className="leadership-container">
         <ul className="leadership-list">
           <li className="leadership-list-item">
-            <span className="leadership-name">Maxim Fateev<br/>CEO</span>
-            <br/>
+            <span className="leadership-name">
+              Maxim Fateev
+              <br />
+              CEO
+            </span>
+            <br />
             <img
               className="leadership-image"
               alt="Maxim Fateev"
               src="https://avatars.githubusercontent.com/u/1463622"
             />
-            <br/>
+            <br />
             <a className="" href="https://twitter.com/mfateev">
               https://twitter.com/mfateev
             </a>
           </li>
           <li className="leadership-list-item">
-            <span className="leadership-name">Charles Zedlewski<br/>CPO</span>
-            <br/>
+            <span className="leadership-name">
+              Charles Zedlewski
+              <br />
+              CPO
+            </span>
+            <br />
             <img
               className="leadership-image"
               alt="Charles Zedlewski"
               src="https://media-exp1.licdn.com/dms/image/C5603AQGgvr2dqvuBsQ/profile-displayphoto-shrink_200_200/0/1610115430759?e=1656547200&v=beta&t=ZXBpqJ6tFg16m2qREtOXbtB0Eylp0fsCkuxHG7tYBbo"
             />
-            <br/>
+            <br />
             <a href="https://twitter.com/zedlewski">
               https://twitter.com/zedlewski
             </a>
           </li>
           <li className="leadership-list-item">
-            <span className="leadership-name">Samar Abbas<br/>CTO</span>
-            <br/>
+            <span className="leadership-name">
+              Samar Abbas
+              <br />
+              CTO
+            </span>
+            <br />
             <img
               className="leadership-image"
               alt="Samar Abbas"
               src="https://avatars2.githubusercontent.com/u/1766515?s=460&u=42e28f95a37b56ef80c55dbaaadd71bf3fc11261&v=4"
             />
-            <br/>
+            <br />
             <a href="https://twitter.com/samarabbas77">
               https://twitter.com/samarabbas77
             </a>
           </li>
           <li className="leadership-list-item">
             <span className="leadership-name">
-              Ryland Goldstein<br/>Head of Product
+              Ryland Goldstein
+              <br />
+              Head of Product
             </span>
-            <br/>
+            <br />
             <img
               className="leadership-image"
               alt="Ryland Goldstein"
               src="https://avatars2.githubusercontent.com/u/27736122?s=460&u=7b6a3e58ec7ed7157f23f51e91a2f4cd2028d606&v=4"
             />
-            <br/>
+            <br />
             <a href="https://twitter.com/taillogs">
               https://twitter.com/taillogs
             </a>
