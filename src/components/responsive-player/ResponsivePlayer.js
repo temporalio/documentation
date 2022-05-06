6b9082d886f4234cfbc5315cb5a4463d3d1fdeb1
Loren ☺️ <251288+lorensr@users.noreply.github.com>
2022-05-06 18:41:20 -0400

    Fix ResponsivePlayer (#1125)

--- src/components/responsive-player/ResponsivePlayer.js
+++ src/components/responsive-player/ResponsivePlayer.js
@@ -5,17 +5,17 @@ function ResponsivePlayer({url, loop, playing}) {
   return (
     <div
       className="relative rounded-lg shadow-lg"
-      style={{paddingTop: "56.25%"}}
+      // https://github.com/CookPete/react-player#responsive-player
+      style={{position: "relative", paddingTop: "56.25%", marginBottom: 20}}
     >
-      {/* /* Player ratio: 100 / (1280 / 720) */}
       <ReactPlayer
-        className="absolute top-0 left-0"
         url={url}
         loop={loop}
         playing={playing}
         width="100%"
         height="100%"
         controls={true}
+        style={{position: "absolute", top: 0, left: 0}}
       />
     </div>
   );
