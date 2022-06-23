d923eab8f0f0949ae9419fa4110b0e9141c39039
Valeri Karpov <val@karpov.io>
2022-06-23 19:03:33 -0400

    add script to set tab language from query string
    Re: #1220

---
+++ static/scripts/set-tab-language.js
@@ -0,0 +1,18 @@
+(function () {
+  const querystring = window.location.search;
+  if (!querystring) {
+    return;
+  }
+
+  const entries = querystring.slice(1).split('&').reduce((entries, str) => {
+    const [key, value] = str.split('=').map(v => decodeURIComponent(v));
+    entries[key] = value;
+    return entries;
+  }, {});
+
+  if (entries.language === 'js') {
+    window.localStorage.setItem('docusaurus.tab.language', 'js');
+  } else if (entries.language === 'ts') {
+    window.localStorage.setItem('docusaurus.tab.language', 'ts');
+  }
+})();
