f284b1f54247fec6f6569076bae221428c25a55b
Cully <cully@temporal.io>
2022-06-28 14:11:00 -0700

    enabling contextual search for versions (#1270)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -208,7 +208,7 @@ module.exports = {
     algolia: {
       apiKey: "cd527863e60d95ebe650cdd21c7a6f3f",
       indexName: "temporal",
-      // contextualSearch: true, // Optional, If you different version of docs etc (v1 and v2) doesn't display dup results
+      contextualSearch: true, // Optional, If you different version of docs etc (v1 and v2) doesn't display dup results
       appId: "T5D6KNJCQS", // Optional, if you run the DocSearch crawler on your own
       // algoliaOptions: {}, // Optional, if provided by Algolia
     },
