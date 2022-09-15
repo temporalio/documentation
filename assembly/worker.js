23b1b3432c0b10ea39e414e32efb0fbe71750d66
Cully <cully@temporal.io>
2022-09-15 14:57:53 -0700

    adding comments to assembly

--- assembly/worker.js
+++ assembly/worker.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
+// remove NativeConnection to use local defaults
 import {Worker, NativeConnection} from "@temporalio/worker";
 import * as activities from "./activities/index.js";
-import fs from "fs-extra";
+// comment fs package to use local defaults
+// import fs from "fs-extra";
 import path from "path";
 
 async function run() {
+  // comment both cert and key to use local defaults
   const cert = await fs.readFile("./secure/docs-assembly.pem");
   const key = await fs.readFile("./secure/docs-assembly.key");
 
+  // comment connection to use local defaults
   const connection = await NativeConnection.connect({
     address: "docs-assembly.a2dd6.tmprl.cloud:7233",
     tls: {
@@ -19,7 +23,9 @@ async function run() {
   });
 
   const worker = await Worker.create({
+    // comment connection to use local defaults
     connection,
+    // comment namespace to use local defaults
     namespace: "docs-assembly.a2dd6",
     workflowsPath: path.resolve("./workflows/fullAssembly.js"),
     activities,
