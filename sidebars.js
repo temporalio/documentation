4de129c342c4e2a6c5353b4647caf82557c4a2ea
Patrick Rachford <prachford@icloud.com>
2022-10-10 15:51:14 -0700

    Dev guide sidebar.js

--- sidebars.js
+++ sidebars.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     {
       type: "category",
-      label: "Developer guide",
+      label: "Dev guide",
       collapsible: true,
       collapsed: true,
       link: {
