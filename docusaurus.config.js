5fac76daf88fa1d08ef37eb506a6c1e1e220fda3
Cully <cully@temporal.io>
2022-11-08 15:31:18 -0700

    fixing domain stripping from docusaurus (#1777)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -198,9 +198,9 @@ module.exports = {
     algolia: {
       apiKey: "4a2fa646f476d7756a7cdc599b625bec",
       indexName: "temporal",
-      // contextualSearch: true, // Optional, If you different version of docs etc (v1 and v2) doesn't display dup results
       appId: "T5D6KNJCQS", // Optional, if you run the DocSearch crawler on your own
-      // algoliaOptions: {}, // Optional, if provided by Algolia
+      // searchParameters: {}, // Optional, if provided by Algolia
+      externalUrlRegex: 'temporal\\.io',
     },
   },
   presets: [
