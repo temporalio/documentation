b5dcde0df4c93ba5678cc32dfb1fc53d2e87558e
Cully <cully@temporal.io>
2022-11-18 14:43:23 -0700

    Update assembly/activities/genLinkIndexes.js
    Co-authored-by: Dail Magee Jr <7518128+djmagee@users.noreply.github.com>

--- assembly/activities/genLinkIndexes.js
+++ assembly/activities/genLinkIndexes.js
@@ -115,7 +115,7 @@ async function generateLinkIndex(guideConfig) {
     try {  
       a_string = a_string.toLowerCase();   
     } catch (e) {
-      throw new Error(`There is missing sidebar metadata in ${id}`); 
+      throw new Error(`Sidebar metadata is missing in ${id}`); 
     }
     a_string = a_string.replaceAll(" ", "-");
     return a_string;
