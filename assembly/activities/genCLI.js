ec1184f86060a8f7b0db0b9e703f6336f732dd52
Samantha Santiago <samantha@temporal.io>
2023-02-03 09:42:48 -0600

    pause for now

--- assembly/activities/genCLI.js
+++ assembly/activities/genCLI.js
@@ -1,20 +1,34 @@
 import {exec} from 'child_process';
+import path from "path";
+import { chmod } from 'fs/promises';
+import { finished } from 'stream/promises';
+
+import fetch from 'node-fetch';
+import zlib from 'zlib';
+import tar from 'tar-fs';
 
 export async function genCLI(config) {
 
     console.log("generating CLI documentation...");
 
+    const FilePathCLI = path.join(
+        config.root_dir,
+        "CLI"
+
+    );
     // put release or link to branch in here
+    const latestRelease = await fetch(
+        'https://github.com/temporalio/cli/tree/auto-generate-cli-docs',
+      ).then((response) => response.json());
+
     // set destination for both the executable
     // and the documents we're generating
 
-    //const response = await fetch(downloadUrl.href);
-    const response = await fetch('https://github.com/temporalio/cli/tree/auto-generate-cli-docs');
-    //const destination = config.rootdir;
     await finished(
-        response.body.pipe(zlib.createGunzip()).pipe(tar.extract(destination)),
+      response.body.pipe(zlib.createGunzip()).pipe(tar.extract(destination)),
     );
-    await chmod(destination, 0o755);
+  
+    await chmod(FilePathCLI, 0o755);
     
     exec('go run ./cmd/doc', (err, stdout, stderr) => {
         if (err) {
