db0caa45550b1d6a61cbb1b8ed34c9913aa8757e
Alex Tideman <alex.tideman@gmail.com>
2023-11-02 13:13:15 -0500

    Update docs landing page with new designs. Add new font, .Net logo

--- docs/components/Intro.js
+++ docs/components/Intro.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "@docusaurus/Link";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import { SdkLogos } from "./SdkLogos";
@@ -44,36 +44,44 @@ const appDevGuideLinks = [
     name: "Go dev guide",
   },
   {
-    path: "/dev-guide/java",
-    name: "Java dev guide",
-  },
-  {
-    path: "/dev-guide/python",
-    name: "Python dev guide",
-  },
-  {
-    path: "/dev-guide/typescript",
-    name: "TypeScript dev guide",
-  },
-  {
-    path: "/dev-guide/php",
-    name: "PHP dev guide",
-  },
-  {
     path: "https://pkg.go.dev/go.temporal.io/sdk",
     name: "Go SDK API reference",
+  },
+  {
+    path: "/dev-guide/java",
+    name: "Java dev guide",
   },
   {
     path: "https://www.javadoc.io/doc/io.temporal/temporal-sdk/latest/index.html",
     name: "Java SDK API reference",
   },
   {
+    path: "/dev-guide/python",
+    name: "Python dev guide",
+  },
+  {
     path: "https://python.temporal.io",
     name: "Python SDK API reference",
   },
   {
+    path: "/dev-guide/typescript",
+    name: "TypeScript dev guide",
+  },
+  {
     path: "https://typescript.temporal.io/",
     name: "TypeScript SDK API reference",
+  },
+  {
+    path: "https://github.com/temporalio/sdk-dotnet#readme",
+    name: ".NET SDK readme",
+  },
+  {
+    path: "https://dotnet.temporal.io/",
+    name: ".NET SDK API reference",
+  },
+  {
+    path: "/dev-guide/php",
+    name: "PHP dev guide",
   },
 ]
 
@@ -145,7 +153,7 @@ const devTools = [
 const references = [
   {
     path: "/references/events",
-    name: "Events",
+    name: "Events reference",
   },
   {
     path: "/references/commands",
@@ -153,11 +161,15 @@ const references = [
   },
   {
     path: "/references/sdk-metrics",
-    name: "SDK metrics",
+    name: "SDK Metrics",
   },
   {
     path: "/references/configuration",
-    name: `Cluster configuration`,
+    name: `Cluster configuration reference`,
+  },
+  {
+    path: "/references/errors",
+    name: `Errors`,
   },
 ];
 
@@ -210,7 +222,6 @@ const selfHostedDeploymentGuide = [
     path: "/kb/cadence-to-temporal",
     name: "Cadence to Temporal migration highlights",
   },
-
 ];
 
 
@@ -228,6 +239,7 @@ const displayLinks = (links) => {
   });
 };
 
+
 function Card({ title, content, links = [] }) {
   return (
     <div className="main-card">
@@ -238,10 +250,21 @@ function Card({ title, content, links = [] }) {
   )
 }
 
+function SimpleCard({ title, content, links = [] }) {
+  return (
+    <div className="main-card">
+      <h1>{title}</h1>
+      <p>{content}</p>
+      <ul className="single-column-list">{displayLinks(links)}</ul>
+    </div>
+  )
+}
+
+
 function CloudCard({ links }) {
   return (
     <div className="main-card">
-      <div class="grid grid-cols-2">
+      <div className="grid grid-cols-1 md:grid-cols-2">
         <div>
           <h1>Temporal Cloud</h1>
           <p>Temporal Cloud tools and how-to guides.</p>
@@ -251,7 +274,7 @@ function CloudCard({ links }) {
           <h1>Get started with Cloud</h1>
           <p>Run Temporal today, without hassle, and with peace of mind.</p>
           <p>
-            <button className="cloud-button">Learn more about Temporal Cloud</button>
+            <button className="cloud-button"><Link to="https://temporal.io/cloud">Learn more about Temporal Cloud</Link></button>
           </p>
           <p>Existing users can <Link to="https://cloud.temporal.io">log in here
           </Link>
@@ -262,41 +285,142 @@ function CloudCard({ links }) {
   )
 }
 
+function ResourceCard({ title, author, role, link }) {
+  return (
+    <div className="main-card resource-card">
+      <a href={link} className="">
+        <h3>{title}</h3>
+      </a>
+      <div className="flex items-center gap-2">
+        <div className="resource-img" />
+        <div className="author">
+          <p><strong>{author}</strong></p>
+          <p>{role}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function CommunityCard({ title, content, link }) {
+  return (
+    <div className="main-card community-card">
+      <h3>{title}</h3>
+      <p>{content}</p>
+      <Link to={useBaseUrl(link.path)} className="hover:underline font-normal">
+        {link.name}
+      </Link>
+    </div>
+  )
+}
+
+const slides = [
+  { title: 'Temporal Cluster', description: 'Provides reliable and scalable workflow orchestration', content1: `A Temporal Cluster ensures that Workflows are resilient by saving each step the Workflow code takes. It also schedules retries whenever there are errors in your application’s code.`, content2: `You can run it wherever you’d like and scale from a single laptop to thousands of machines, adding resources when and where you need them. And although you've just set up a lightweight Cluster for development, switching your code to point at a production Cluster only requires changing your connection parameters.`, giphy: 'eePSFNBFv2W9owZ4Sh' },
+  { title: 'SDKs', description: 'Write code in your programming language of choice.', content1: 'A Temporal SDK is a library you use in your code. Each SDK has two parts to its API: the Client and the Worker. The Worker is the long-running process that has your Workflow code. The Client is used to communicate with the Cluster—for instance, to start and interact with Workflows.', giphy: '6UFgdU9hirj1pAOJyN' },
+  { title: 'Workflows', description: 'Write your business logic as code in a Temporal Workflow.', content1: `Temporal applications are built using an abstraction called Workflows. Workflows are functions or methods written in a general-purpose programming language such as Go, Java, TypeScript, or Python.`, content2: `Temporal ensures that your Workflows execute reliably. They can run—and keep running—for years, even if the underlying infrastructure fails. If the application crashes, Temporal will automatically recreate its pre-failure state so that it can continue right where it left off.`, giphy: 'l0IylOPCNkiqOgMyA ' },
+  { title: 'Determinism', description: 'Writing deterministic Workflows guarantees durability in the case of failure.', content1: `Temporal's guarantee that your Workflow code can’t fail to continue running requires that the code in your Workflow must be deterministic. Simply put, this means that each execution of a Workflow, given the same input, must follow the same path through the code and produce the same output.`, content2: `This means that you can’t generate a random number or interact with the outside world, as these actions are inherently non-deterministic. Generating a random number twice will likely produce different results, and if you access files, databases, or network services, you might get a failure or different results at different times.`, giphy: '5FSanfKqd3rS8' },
+  { title: 'Activities', description: 'Activities have the failure-prone parts of your code and are automatically retried upon failure.', content1: 'Activities are functions or methods that you can use to run code that is non-deterministic or prone to failure, such as accessing a database, network service, or file system. Activities are called from Workflow code, and they are automatically retried if they fail. Once the Activity succeeds, the Workflow code will continue executing.', giphy: 'kaDAIEecq0YuI' },
+  { title: 'Workers', description: 'Workers run Workflow and Activity code reliably and consistently. You can increase the scalability and availability of your application by running additional Workers. ', content1: `One thing that people new to Temporal find surprising is that the Temporal Cluster does not execute your code.`, content2: `You deploy your code to containers or machines that are separate from the Temporal Cluster. Using the Worker API provided by the Temporal SDK, you will run one or more Worker processes that will execute your Workflow and Activity code based on tasks managed by the Temporal Cluster.`, content3: `The Cluster gives the Workers tasks like “Start running Workflow A,” and the Workers run the corresponding Workflow function or method.`, giphy: 'DhstvI3zZ598Nb1rFf' },
+  {
+    title: 'Temporal CLI', description: 'Communicate directly with the Temporal Cluster by entering commands in the terminal.', content1: `The Temporal CLI runs a development version of the Cluster. Interact with the Cluster (start Workflows, list them, get their status, and more):`, content2: `  From the command line using the temporal command. From code, using an SDK Client. From your browser, using the Web UI`, giphy: 'GA2dwDU7owOS4'
+  },
+]
+function SlideCarousel({ handleMouseOver }) {
+  return slides.map((link, index) => {
+    return (<Slide link={link.giphy} key={link.giphy} handleMouseOver={() => handleMouseOver(index)} />)
+  })
+}
+
+function SlideDots({ activeSlide, handleMouseOver }) {
+  return slides.map((link, index) => {
+    return (<div key={index} className={`slide-dot ${activeSlide === index ? 'slide-dot-active' : ''}`} onMouseOver={() => handleMouseOver(index)} />)
+  })
+}
+
+function SlideInfo({ activeSlide }) {
+  const slide = slides[activeSlide]
+  return (
+    <div className="core-concepts-info">
+      <h3 className="core-concepts-title">Introducing our core concepts</h3>
+      <h1>{slide.title}</h1>
+      <h3 className="core-concepts-description">{slide.description}</h3>
+      <p>{slide.content1}</p>
+      <p>{slide?.content2}</p>
+      <p>{slide?.content3}</p>
+    </div>
+  )
+}
+function Slide({ link, handleMouseOver }) {
+  return (
+    <div className="slide" onMouseOver={handleMouseOver}>
+      <iframe src={`https://giphy.com/embed/${link}`} width="100%" height="100%" allowFullScreen></iframe>
+    </div>
+  )
+}
+
 
 function Explained() {
+  const [activeSlide, setActiveSlide] = useState(0)
+
+  const handleSlideChange = (index) => {
+    setActiveSlide(index)
+  }
+
   return (
     <div>
       <p className="main-subhead">Temporal Platform explanation and usage information.</p>
-      <div className="">{SdkLogos()}</div>
-
-      <div className="flex gap-6">
+      <div className="mb-8">{SdkLogos()}</div>
+      <div className="grid md:grid-cols-2">
         <Card title="Get Started with Temporal" content="New to Temporal? Start your journey here by setting up your development environment, running an existing Temporal app, and then building your first app from scratch using our SDKs." links={coreConcepts} />
         <Card title="Start building your next app" links={appDevGuideLinks} />
       </div>
-
       <CloudCard links={cloudInfo} />
-
-      <div className="flex gap-6">
-        <Card title="Production readiness" content="Temporal Cluster deployment information and how-to guides." links={clusterGuide} />
-        <Card title="Dev tools" content="Use Temporal’s CLI or Web UI to manage and monitor your Workflows." links={devTools} />
+      <div className="grid md:grid-cols-2">
+        <SimpleCard title="Production readiness" content="Temporal Cluster deployment information and how-to guides." links={clusterGuide} />
+        <SimpleCard title="Dev tools" content="Use Temporal’s CLI or Web UI to manage and monitor your Workflows." links={devTools} />
+      </div>
+      <div className="my-20 flex gap-6 core-concepts">
+        <div className="slide-dots">
+          <SlideDots activeSlide={activeSlide} handleMouseOver={handleSlideChange} />
+        </div>
+        <SlideInfo activeSlide={activeSlide} />
+        <div className="core-concepts-slide">
+          <SlideCarousel handleMouseOver={handleSlideChange} />
+        </div>
       </div>
 
-      <div className="my-20">
-        <h3 className="core-concepts-title">Introducing our core concepts</h3>
-        <h1>Temporal Cluster</h1>
-      </div>
-
-      <div className="flex gap-6">
-        <Card title="References" links={references} />
+      <div className="flex items-start gap-2">
+        <div className="w-third">
+          <SimpleCard title="References" links={references} />
+        </div>
         <Card title="Self-hosted deployment guide" links={selfHostedDeploymentGuide} />
       </div>
 
       <div className="my-20 text-center">
         <h1>Helpful resources</h1>
+        <div className="flex items-center gap-6">
+          <ResourceCard title="Introduction to Temporal Workflows" author="Dominik Tornow" role="Principle Engineer" link="https://temporal.io/blog/dominik-workflow-part-1" />
+          <ResourceCard title="Failure Handling in Practice" author="Fitz" role="Developer Advocate" link="https://temporal.io/blog/failure-handling-in-practice" />
+          <ResourceCard title="Time-Travel Debugging Production Code" author="Loren Sands-Ramshaw" role="Developer Relations Engineer" link="https://temporal.io/blog/time-travel-debugging-production-code" />
+        </div>
       </div>
 
       <div className="my-20 text-center">
         <h1>Join our Community</h1>
+        <div className="flex gap-6">
+          <CommunityCard title="Join us in Slack and say hi!" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut quam id nisl malesuada scelerisque. " link={{
+            path: "https://temporal.io/slack",
+            name: "Launch Slack",
+          }} />
+          <CommunityCard title="Got a Question? Check out the forum" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut quam id nisl malesuada scelerisque." link={{
+            path: "https://community.temporal.io/",
+            name: "Go to the community forum",
+          }} />
+          <CommunityCard title="Find an upcoming event near you" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut quam id nisl malesuada scelerisque." link={{
+            path: "https://temporal.io/community",
+            name: "Check out upcoming events",
+          }} />
+        </div>
       </div>
 
     </div>
@@ -306,7 +430,7 @@ function Explained() {
 export const Intro = () => {
   return (
     <section className="mt-1 mb-14 tailwindcss">
-      <div className=" grid grid-cols-1 gap-6 lg:gap-8">
+      <div className="main-grid grid grid-cols-1 gap-6 lg:gap-8">
         <Explained />
       </div>
     </section>
