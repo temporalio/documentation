cdf2fd6a1a0a60fc676d0f203429906e517d5b58
Erica Sadun <erica.sadun@temporal.io>
2024-04-11 09:40:05 -0600

    Updated to move activity and retry options into Workflow implementation
    * Externalized specifics to the Workflow so each Workflow owns
      and controls that behavior.
    * Made these final with consult with Mason

--- sample-apps/java/client/devserver-client-sample/src/main/java/timefetch/TimeActivities.java
+++ sample-apps/java/client/devserver-client-sample/src/main/java/timefetch/TimeActivities.java
@@ -1,28 +1,9 @@
 package timefetch;
 
-import java.time.Duration;
 import io.temporal.activity.Activity;
 import io.temporal.activity.ActivityInterface;
-import io.temporal.activity.ActivityOptions;
-import io.temporal.common.RetryOptions;
 
 @ActivityInterface
 public interface TimeActivities {
     public String getTime();
-    // Calls closer together than 4 seconds will be considered
-    // a possible denial of service attack. Set initial interval
-    // to 5 seconds, and do not increase.
-    public RetryOptions retryOptions = RetryOptions.newBuilder()
-        .setInitialInterval(Duration.ofSeconds(5))
-        .setBackoffCoefficient(1.0)
-        .build();
-
-    public ActivityOptions activityOptions = ActivityOptions
-        .newBuilder()
-        // Max Activity execution time
-        .setStartToCloseTimeout(Duration.ofSeconds(60))
-        // Entire duration from scheduling to completion, including queue time.
-        .setScheduleToCloseTimeout(Duration.ofSeconds(90))
-        .setRetryOptions(retryOptions)
-        .build();
 }
