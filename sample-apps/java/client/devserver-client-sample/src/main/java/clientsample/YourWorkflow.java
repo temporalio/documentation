9e331df6fcf6d92a1f0ae40de275f1d3640122d7
Erica Sadun <erica.sadun@temporal.io>
2024-04-12 11:56:43 -0600

    EDU-2214: Removed artifacts of EDU-2212

--- sample-apps/java/client/devserver-client-sample/src/main/java/clientsample/YourWorkflow.java
+++
@@ -1,9 +0,0 @@
-package clientsample;
-
-import io.temporal.workflow.WorkflowInterface;
-import io.temporal.workflow.WorkflowMethod;
-
-@WorkflowInterface
-public interface YourWorkflow {
-    @WorkflowMethod String initiateWorkflow();
-}
