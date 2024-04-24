55f6d440183c1e16879546b70f87942eda192692
Cully Wakelin <cully@temporal.io>
2024-04-24 15:56:54 -0600

    remove duplicate content, convert to temporal client examples to snipsync

--- sample-apps/go/cloud/client/main.go
+++ sample-apps/go/cloud/client/main.go
@@ -1,3 +1,4 @@
+// @@@SNIPSTART sample-apps-go-cloud-client
 package main
 
 import (
@@ -10,19 +11,6 @@ import (
 
 	"go.temporal.io/sdk/client"
 )
-
-/*
-To connect to and run Workflows through Temporal Cloud, you need the following:
-- A compatible mTLS CA certificate and mTLS private key that has been added to your Namespace.
-See [certificate requirements](/cloud/certificates-requirements).
-- Your [Temporal Cloud Namespace Id](/concepts/what-is-a-cloud-namespace-id), which includes your [Temporal Cloud Namespace Name](/concepts/what-is-a-cloud-namespace-name) and the unique five- or six-digit [Temporal Cloud Account Id](/concepts/what-is-a-cloud-account-id) that is appended to it.
-This information can be found in the URL of your Namespace; for example, `https://cloud.temporal.io/namespaces/yournamespace.a2fx6/`.
-Remember that the Namespace Id must include the Account Id: `yournamespace.a2fx6`.
-
-For more information about managing and generating client certificates for Temporal Cloud, see [How to manage certificates in Temporal Cloud](/cloud/certificates-intro).
-
-For more information about configuring TLS to secure inter- and intra-network communication for a Temporal Cluster, see [Temporal Customization Samples](https://github.com/temporalio/samples-server).
-*/
 
 func main() {
 	// Get the key and cert from your env or local machine
@@ -76,14 +64,4 @@ func main() {
 	log.Println(string(b))
 }
 
-/* @dacx
-id: how-to-connect-to-temporal-cloud-in-go
-title: How to connect to Temporal Cloud
-label: Connect to Temporal Cloud
-description: Use a certificate key pair and your Temporal Cloud Namespace to connect to Temporal Cloud.
-tags:
-- go sdk
-- code sample
-- temporal cloud
-lines: 1-51, 77
-@dacx */
+// @@@SNIPEND
