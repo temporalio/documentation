8538bb571231d01ae96c3dc39522b1605486fd7d
Erica Sadun <156466156+fairlydurable@users.noreply.github.com>
2024-04-29 17:22:01 -0600

    EDU 2193: Populate Development > Java SDK > Temporal Client (#2752)
    * EDU-2193 - Populate Development > Java SDK > Temporal Client with how-to.
    
    https://temporalio.atlassian.net/browse/EDU-2193
    
    * EDU-2193: Populate Development > Java SDK > Temporal Client
    
    JIRA: https://temporalio.atlassian.net/browse/EDU-2193
    
    * EDU-2193: Update to remove non-Workflow clients
    
    * Remove all other clients
    * Perform Vale check
    
    The next step will be to integrate SnipSync and close the epic
    
    JIRA: https://temporalio.atlassian.net/browse/EDU-2193
    
    * EDU-2193: Remove gRPC and WorkerFactory explanations
    
    JIRA: https://temporalio.atlassian.net/browse/EDU-2193
    
    * EDU-2193: Updated with Cully's new outline
    
    JIRA: https://temporalio.atlassian.net/browse/EDU-2193
    
    * EDU-2193: Remove environment variable coverage as unneeded
    
    JIRA: https://temporalio.atlassian.net/browse/EDU-2193
    
    * rm duplicate foundations content, updates links
    
    * Update docs/dev-guide/javalang/features.mdx
    
    * seo description & keywords
    
    ---------
    
    Co-authored-by: Cully Wakelin <cully@temporal.io>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -210,7 +210,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           routeBasePath: "/",
-          exclude: ["**/php/**", "**/typescript/**", "**/java/**", "**/clusters/**"], // do not render context content
+          exclude: ["**/php/**", "**/typescript/**", "**/clusters/**"], // do not render context content
           editUrl: "https://github.com/temporalio/documentation/blob/master",
           /**
            * Whether to display the author who last updated the doc.
