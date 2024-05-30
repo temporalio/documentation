0f1b9a149302e87fe3c161ded10cf356825d6840
Erica Sadun <156466156+fairlydurable@users.noreply.github.com>
2024-05-30 09:30:35 -0600

    DO NOT MERGE: EDU-1180 Multi-region Namespace Epic (#2856)
    * EDU-1181: Add starting image assets for MRN pages
    
    * EDU-1181: Load main multi-region page
    
    * EDU-1181: Added in sidebar.js, updated link for admonition
    
    * EDU-1181 - Style update
    
    * Shortened H2 titles
    * Added bolded qs under H2s
    * Checked all links
    * Fixed one broken link
    
    * EDU-2387: MRN Glossary entries
    
    * Multi-region Namespace
    * Failover
    * Updated Global Namespace
    
    * EDU-2442: Update Audit Logging with Failover event
    
    * EDU-2427: add multi-region operations to tcld coverage
    
    * EDU-1180: Update `FailoverNamespaces` to `FailoverNamespace`
    
    * EDU-2443: Add metrics for MRN
    
    * EDU-2443: Re-order items alphabetically for new MRN metrics
    
    * EDU-2443: Add metrics to main MRN coverage
    
    * Fixing typo 'cloud' is now 'Cloud' in MRN page metrics
    
    * EDU-2385: Initial commit with base content
    
    * Add admonition for MRN to Evaluate > Prod > Temporal Cloud > High Availability
    
    * EDU-2495: Add SLA updates for MRN
    
    * EDU-1569 EDU-2450 Pricing Tickets
    
    * EDU-1181 | EDU-2443: Update Metrics coverage in MRN landing page.
    
    * Update docs/production-deployment/cloud/introduction/pricing.mdx
    
    * Fixing unbalanced parens and making note stand out
    
    * EDU-1181: Responds to feedback from Irina
    
    * EDU-1181: Responds to feedback from Irina
    
    * EDU-1181: Incorporates feedback from Jim about Action pricing
    
    * Updates SLA wording (Nikitha)
    
    * One more Activity pricing tweak for clarity
    
    * Corrects "counts" to "count" in two places on pricing page
    
    * Responds to Irina's concerns about "highest level of availability"
    
    * Rewords "Should you choose" section with help from Irina
    
    * Responding to Nikitha requests
    
    * Update docs/evaluate/production-features/self-hosted/high-availability.mdx
    
    * Removes oddball duplication
    
    * Update docs/evaluate/production-features/temporal-cloud/high-availability.mdx
    
    * Update docs/evaluate/production-features/temporal-cloud/high-availability.mdx
    
    * Update docs/production-deployment/cloud/introduction/sla.mdx
    
    * Update docs/evaluate/production-features/index.mdx
    
    Co-authored-by: Patrick Rachford <64233065+rachfop@users.noreply.github.com>
    
    * Update docs/evaluate/production-features/index.mdx
    
    Co-authored-by: Patrick Rachford <64233065+rachfop@users.noreply.github.com>
    
    * Update docs/evaluate/production-features/temporal-cloud/high-availability.mdx
    
    Co-authored-by: Patrick Rachford <64233065+rachfop@users.noreply.github.com>
    
    * Update docs/evaluate/production-features/temporal-cloud/index.mdx
    
    * Update docs/evaluate/production-features/temporal-cloud/index.mdx
    
    * Update docs/evaluate/production-features/temporal-cloud/index.mdx
    
    Co-authored-by: Patrick Rachford <64233065+rachfop@users.noreply.github.com>
    
    * Update docs/glossary.md
    
    * Update docs/production-deployment/cloud/introduction/sla.mdx
    
    Co-authored-by: Patrick Rachford <64233065+rachfop@users.noreply.github.com>
    
    * Update docs/production-deployment/cloud/tcld/namespace.mdx
    
    Co-authored-by: Patrick Rachford <64233065+rachfop@users.noreply.github.com>
    
    * Update docs/production-deployment/cloud/tcld/namespace.mdx
    
    * Update docs/production-deployment/cloud/tcld/namespace.mdx
    
    * Update docs/production-deployment/cloud/tcld/namespace.mdx
    
    * Update docs/production-deployment/cloud/introduction/sla.mdx
    
    Co-authored-by: Patrick Rachford <64233065+rachfop@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Patrick Rachford <64233065+rachfop@users.noreply.github.com>

--- sidebars.js
+++ sidebars.js
@@ -26,6 +26,37 @@ module.exports = {
             "evaluate/development-features/throughput-composability",
           ],
         },
+        {
+          type: "category",
+          label: "Production features",
+          collapsed: true,
+          link: {
+            type: "doc",
+            id: "evaluate/production-features/index",
+          },
+          items: [
+            {
+              type: "category",
+              label: "Temporal Cloud",
+              collapsed: true,
+              link: {
+                type: "doc",
+                id: "evaluate/production-features/temporal-cloud/index",
+              },
+              items: ["evaluate/production-features/temporal-cloud/high-availability"],
+            },
+            {
+              type: "category",
+              label: "Self-hosted",
+              collapsed: true,
+              link: {
+                type: "doc",
+                id: "evaluate/production-features/self-hosted/index",
+              },
+              items: ["evaluate/production-features/self-hosted/high-availability"],
+            },
+          ],
+        },
         "security",
         "evaluate/release-stages",
         {
@@ -250,6 +281,7 @@ module.exports = {
                 "production-deployment/cloud/metrics/prometheus-grafana",
               ],
             },
+            "production-deployment/cloud/multi-region",
             "production-deployment/cloud/worker-health",
             "production-deployment/cloud/saml",
             "production-deployment/cloud/operation-api",
