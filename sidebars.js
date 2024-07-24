6af0c8baa19ccd4998fc863c02ce7dd748455212
Jwahir Sundai <jwahir.sundai@temporal.io>
2024-07-24 10:38:44 -0500

    update folder and edit based on review

--- sidebars.js
+++ sidebars.js
@@ -26,7 +26,7 @@ module.exports = {
             "evaluate/development-production-features/failure-detection",
             "evaluate/development-production-features/throughput-composability",
             "evaluate/development-production-features/workflow-message-passing",
-            "evaluate/development-features/interrupt-workflow",
+            "evaluate/development-production-features/interrupt-workflow",
             "evaluate/development-production-features/high-availability",
             {
               type: "category",
