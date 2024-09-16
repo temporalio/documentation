5c5b8a1cbd9d79dc37e7062452199cc6b871ce25
Jwahir Sundai <jwahir.sundai@temporal.io>
2024-09-16 17:46:51 -0500

    Temporal Nexus: Landing page under Evaluate content  (#3052)
    * first add for nexus landing page
    
    * change title
    
    * adding messaging content
    
    * adding related read container - no links yet
    
    * remove repetitive sentence
    
    * adding in more content from messaging guide
    
    * removing based on past convos
    
    * changing tags
    
    * removing banner
    
    * adding new landing page
    
    * make small edits with breaking up sentences and removing anything
     that dates the docs
    
    * adding sentence into paragraph
    
    * adding go button
    
    * adding line at top
    
    * small fix
    
    * fixing link
    
    * adding more edits and banner
    
    * additional edits
    
    * is
    
    * adding links as placeholder
    
    * small stylistic edits
    
    * title change
    
    * both/with

--- sidebars.js
+++ sidebars.js
@@ -25,6 +25,7 @@ module.exports = {
             "evaluate/development-production-features/temporal-client",
             "evaluate/development-production-features/failure-detection",
             "evaluate/development-production-features/throughput-composability",
+            "evaluate/development-production-features/nexus",
             "evaluate/development-production-features/workflow-message-passing",
             "evaluate/development-production-features/debugging",
             "evaluate/development-production-features/interrupt-workflow",
