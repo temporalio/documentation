ffd7c5cf8acf7765bd4b936ba22a33f8126b895d
Jwahir Sundai <jwahir.sundai@temporal.io>
2024-10-17 10:38:36 -0500

    rename (#3163)

--- sidebars.js
+++ sidebars.js
@@ -11,7 +11,7 @@ module.exports = {
       },
       items: [
         "evaluate/why-temporal",
-        "evaluate/how-temporal-works",
+        "evaluate/understanding-temporal",
         {
           type: "category",
           label: "Features",
