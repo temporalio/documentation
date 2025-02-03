cf6c2c1caa42cf218cbb22e35b8ab7cb7837ec19
Erica Sadun <erica.sadun@temporal.io>
2025-02-03 13:33:55 -0700

    EDU-3661: Monday Feb 3 Updates with Nikitha feedback
    - Some significant shifting of content
    - Info should be more readily consumed in order
    - Landing page is still not reconciled

--- sidebars.js
+++ sidebars.js
@@ -348,9 +348,9 @@ module.exports = {
               },
               items: [
                 "production-deployment/cloud/high-availability/enable",
+                "production-deployment/cloud/high-availability/failovers",
                 "production-deployment/cloud/high-availability/how-it-works",
-                "production-deployment/cloud/high-availability/guarantees",
-                "production-deployment/cloud/high-availability/monitor",
+                "production-deployment/cloud/high-availability/best-practices",
                 "production-deployment/cloud/high-availability/faq",
               ],
             },
