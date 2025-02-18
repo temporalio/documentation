f31ae06e32a49bc15a21a41d0f721808c144624f
Erica Sadun <erica.sadun@temporal.io>
2025-02-18 14:42:09 -0700

    With horrible horrible concepts page. IGNORE IT

--- sidebars.js
+++ sidebars.js
@@ -336,9 +336,10 @@ module.exports = {
               items: [
                 "production-deployment/cloud/high-availability/how-it-works",
                 "production-deployment/cloud/high-availability/enable",
-                // "production-deployment/cloud/high-availability/guarantees",
                 "production-deployment/cloud/high-availability/monitor",
                 "production-deployment/cloud/high-availability/failovers",
+                "production-deployment/cloud/high-availability/guarantees",
+                "production-deployment/cloud/high-availability/concepts",
                 // "production-deployment/cloud/high-availability/faq",
               ],
             },
