41f02a4c2190b7895cff5c56aa6c8298959c8802
angelazhou32 <141692465+angelazhou32@users.noreply.github.com>
2025-04-17 15:10:07 -0400

    adds photo carousel react component and first part of ts walkthrough … (#3513)
    * adds photo carousel react component and first part of ts walkthrough of code to commands
    
    * ts code walkthrough and carousel improveements
    
    * remove comments to clean up photocarousel file
    
    * takes bmacs suggestions
    
    * edits photocarousel so can enlarge with larger size
    
    * most recent photocarousel changes
    
    * adds rest of pages
    
    * fix errors
    
    * fix error
    
    * adds feedback from tiho
    
    * brings in tihos changes
    
    * incorporates more feedback
    
    * change organization
    
    ---------
    
    Co-authored-by: Jwahir Sundai <jwahir.sundai@temporal.io>

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,18 @@
 // Website components
-export { default as RetrySimulator } from './elements/RetrySimulator';
-export { Intro } from './elements/Intro';
-export { SdkLogos } from './elements/SdkLogos';
+export { default as RetrySimulator } from "./elements/RetrySimulator";
+export { Intro } from "./elements/Intro";
+export { SdkLogos } from "./elements/SdkLogos";
+export { default as PhotoCarousel } from './elements/PhotoCarousel';
 
 // Formatting components
-export { default as DocsTable, NewDocsCell } from './formatting/DocsTable';
+export { default as DocsTable, NewDocsCell } from "./formatting/DocsTable";
 
 // Image components
-export { default as CaptionedImage } from './images/CaptionedImage';
-export { default as ZoomingImage } from './images/ZoomingImage';
+export { default as CaptionedImage } from "./images/CaptionedImage";
+export { default as ZoomingImage } from "./images/ZoomingImage";
 
 // Information components
-export { default as DiscoverableDisclosure } from './info/DiscoverableDisclosure';
-export { default as ToolTipTerm } from './info/ToolTipTerm';
-export { RelatedReadContainer, RelatedReadItem } from './info/RelatedRead';
-export { default as RelatedReadList } from './info/RelatedReadList';
+export { default as DiscoverableDisclosure } from "./info/DiscoverableDisclosure";
+export { default as ToolTipTerm } from "./info/ToolTipTerm";
+export { RelatedReadContainer, RelatedReadItem } from "./info/RelatedRead";
+export { default as RelatedReadList } from "./info/RelatedReadList";
