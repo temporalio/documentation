2d57819b78484d856eae3f761fae8d4c52382cf6
Alex Thomsen <alex.scott.thomsen@gmail.com>
2025-04-24 15:31:25 -0600

    Drop iife

--- src/client/remote-amplitude-analytics.js
+++ src/client/remote-amplitude-analytics.js
@@ -9,33 +9,31 @@ if (ExecutionEnvironment.canUseDOM) {
   analytics = new RemoteCommsClass(url, window.document);
 }
 
-export function onRouteDidUpdate() {
-  (async () => {
-    if (ExecutionEnvironment.canUseDOM && analytics) {
-      try {
-        if (window.amplitude) {
-          /** If we don't have a userId in amplitude refresh the remote amplitude iFrame and get the userId and
-           * set it if it exists, this will merge both device ID's */
-          if (!window.amplitude.getUserId()) {
-            analytics.refreshComms();
-            const userid = await analytics.getUserId();
+export async function onRouteDidUpdate() {
+  if (ExecutionEnvironment.canUseDOM && analytics) {
+    try {
+      if (window.amplitude) {
+        /** If we don't have a userId in amplitude refresh the remote amplitude iFrame and get the userId and
+         * set it if it exists, this will merge both device ID's */
+        if (!window.amplitude.getUserId()) {
+          analytics.refreshComms();
+          const userid = await analytics.getUserId();
 
-            // Only update userId if we actually get something from the frame
-            if (userid) {
-              window.amplitude.setUserId(userid);
-            }
-          }
-
-          // This might cause weird sessionID stuff but if amplitude exists we'll nab the remote one quickly
-          // probably only gonna lose the first event if we event send an event
-          const sessionId = await analytics.getSessionId();
-          if (sessionId) {
-            window.amplitude.setSessionId(sessionId);
+          // Only update userId if we actually get something from the frame
+          if (userid) {
+            window.amplitude.setUserId(userid);
           }
         }
-      } catch (e) {
-        console.log(e);
+
+        // This might cause weird sessionID stuff but if amplitude exists we'll nab the remote one quickly
+        // probably only gonna lose the first event if we event send an event
+        const sessionId = await analytics.getSessionId();
+        if (sessionId) {
+          window.amplitude.setSessionId(sessionId);
+        }
       }
+    } catch (e) {
+      console.log(e);
     }
-  })();
+  }
 }
