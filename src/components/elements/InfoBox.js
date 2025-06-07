d549805bd748df2447da4cbe9319c4b1e8ebf4c4
Jwahir Sundai <jwahir.sundai@temporal.io>
2025-06-07 10:16:38 -0400

    first draft of set up page with new visual style and code blocks to side of page

---
+++ src/components/elements/InfoBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './info-box.module.css';
+
+export const InfoBox = ({ title, children }) => {
+  return (
+    <div className={styles.infoBox}>
+      <div className={styles.infoIcon}>ⓘ</div>
+      <div className={styles.content}>
+        <h3>{title}</h3>
+        {children}
+      </div>
+    </div>
+  );
+}; 
