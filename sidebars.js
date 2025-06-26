cd9deb378c720027025341d2826b0ae4f0a4e8bd
Andrew Yuan <theandrewyuan@gmail.com>
2025-06-26 16:20:23 -0700

    New worker shutdown behavior page (#3621)
    * add worker-shutdown page
    
    * CI: Automatic .md and .mdx formatting
    
    * capitalization
    
    * formatting
    
    * CI: Automatic .md and .mdx formatting
    
    * Update worker-shutdown.mdx
    
    * CI: Automatic .md and .mdx formatting
    
    * Update worker-shutdown.mdx
    
    * CI: Automatic .md and .mdx formatting
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Jwahir Sundai <jwahir.sundai@temporal.io>

--- sidebars.js
+++ sidebars.js
@@ -625,6 +625,7 @@ module.exports = {
             "encyclopedia/workers/task-routing-worker-sessions",
             "encyclopedia/workers/sticky-execution",
             "encyclopedia/workers/worker-deployments",
+            "encyclopedia/workers/worker-shutdown",
             "encyclopedia/workers/worker-versioning",
           ],
         },
