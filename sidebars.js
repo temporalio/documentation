68a9b4f5c016c04a7a10147f43c73003b0feb03f
Ben Echols <benjamin.echols@temporal.io>
2025-07-16 19:52:44 -0700

    update eval pages

--- sidebars.js
+++ sidebars.js
@@ -35,8 +35,6 @@ module.exports = {
             "evaluate/development-production-features/cloud-automation",
             "evaluate/development-production-features/low-latency",
             "evaluate/development-production-features/multi-tenancy",
-            "evaluate/development-production-features/multi-region-namespace",
-            "evaluate/development-production-features/multi-cloud-namespace",
             {
               type: "category",
               label: "Product release stages",
@@ -85,6 +83,8 @@ module.exports = {
             "evaluate/temporal-cloud/pricing",
             "evaluate/temporal-cloud/actions",
             "evaluate/temporal-cloud/support",
+            "evaluate/development-production-features/multi-region-namespace",
+            "evaluate/development-production-features/multi-cloud-namespace",
           ],
         },
         "security",
