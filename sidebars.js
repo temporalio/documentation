a1855c921228bdbaa5c3f9473223d3915613d024
Ben Echols <benjamin.echols@temporal.io>
2025-08-01 13:44:27 -0700

    HA cleanup (#3728)
    * HA doc cleanup
    
    * update sidebar
    
    * fix typo
    
    * reduce duplication
    
    * wording
    
    * CI: Automatic .md and .mdx formatting
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Brian MacDonald <brian.macdonald@temporal.io>

--- sidebars.js
+++ sidebars.js
@@ -359,7 +359,7 @@ module.exports = {
             },
             {
               type: "category",
-              label: "High Availabilty",
+              label: "High Availability",
               collapsed: true,
               link: {
                 type: "doc",
@@ -383,22 +383,6 @@ module.exports = {
                     "production-deployment/cloud/high-availability/how-to/secure-routing",
                     "production-deployment/cloud/high-availability/how-to/private-link",
                     "production-deployment/cloud/high-availability/how-to/migrate",
-                  ],
-                },
-                {
-                  type: "category",
-                  label: "References",
-                  collapsed: true,
-                  link: {
-                    type: "doc",
-                    id: "production-deployment/cloud/high-availability/references/index",
-                  },
-                  items: [
-                    "production-deployment/cloud/high-availability/references/regions",
-                    // "production-deployment/cloud/high-availability/references/concepts",
-                    // "production-deployment/cloud/high-availability/references/best-practices",
-                    // "production-deployment/cloud/high-availability/references/glossary",
-                    // "production-deployment/cloud/high-availability/references/faq",
                   ],
                 },
               ],
