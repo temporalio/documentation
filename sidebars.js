50e91b65251bcbd450b609fed49c557803a565ac
flippedcoder <milecia.mcgregor@aol.com>
2025-12-17 13:20:09 -0600

    feat(task queues): created initial draft for priority and fairness

--- sidebars.js
+++ sidebars.js
@@ -332,6 +332,7 @@ module.exports = {
         'develop/worker-performance',
         'develop/safe-deployments',
         'develop/plugins-guide',
+        'develop/task-queue-priority-fairness',
       ],
     },
     {
