7b01231dafee344f5ed2bc87a3aa0fce4ecd384e
Bilal Karim <4129613+bilal-karim@users.noreply.github.com>
2026-01-12 12:08:26 -0500

    Add action buttons for LLMs

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -5,12 +5,24 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
- 
+import LLMActions from '@site/src/components/LLMActions/LLMActions';
+
+function H1WithLLMActions(props: React.ComponentProps<'h1'>): JSX.Element {
+  return (
+    <>
+      <h1 {...props} />
+      <LLMActions />
+    </>
+  );
+}
+
 export default {
     ...MDXComponents,
     Tabs,
     TabItem,
+    h1: H1WithLLMActions,
 };
