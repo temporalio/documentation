b5ed2ed8458effd4f2fc77f52382c8a48eafb2ed
flippedcoder <milecia.mcgregor@aol.com>
2026-07-20 10:28:41 -0500

    refactor(components): grouped the remaining sdk components together and cleaned up references

--- src/components/index.js
+++ src/components/index.js
@@ -6,10 +6,4 @@ export * from './Quickstart'
 export * from './ToolTipTerm';
 
 export { default as HomePageHero } from './elements/HomePageHero';
-export { SdkLogosAsBlocks } from './elements/SdkLogosAsBlocks';
-export { SdkGuideLinks } from './elements/SdkGuideLinks';
 export { default as PhotoCarousel } from './elements/PhotoCarousel';
-export { default as SdkTabs } from './elements/SdkTabs';
-
-// Extra export
-export { default } from './elements/SdkTabs';
