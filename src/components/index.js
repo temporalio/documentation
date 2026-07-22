82145f85434417096ba7642be6463533ac2a2e96
flippedcoder <milecia.mcgregor@aol.com>
2026-07-22 09:27:33 -0500

    fix(architecture): fixed merge conflicts

--- src/components/index.js
+++ src/components/index.js
@@ -7,4 +7,3 @@ export * from './ToolTipTerm';
 
 export { default as HomePageHero } from './elements/HomePageHero';
 export { default as PhotoCarousel } from './elements/PhotoCarousel';
-export { default as TemporalLifecycleDemo } from './elements/LifecycleDemo/TemporalLifecycleDemo';
