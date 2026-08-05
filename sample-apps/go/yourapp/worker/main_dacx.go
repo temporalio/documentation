50f6bb5f1f73d8e062ab3e95e4044501b1544a23
flippedcoder <milecia.mcgregor@aol.com>
2026-08-05 13:48:24 -0500

    refactor(sample apps): migrated code snippets for go activity basics

--- sample-apps/go/yourapp/worker/main_dacx.go
+++ sample-apps/go/yourapp/worker/main_dacx.go
@@ -11,20 +11,6 @@ import (
 	"documentation-samples-go/yourapp"
 )
 
-/*
-Create an instance of [`Worker`](https://pkg.go.dev/go.temporal.io/sdk/worker#Worker) by calling [`worker.New()`](https://pkg.go.dev/go.temporal.io/sdk/worker#New), available through the `go.temporal.io/sdk/worker` package, and pass it the following parameters:
-
-1. An instance of the Temporal Go SDK `Client`.
-1. The name of the Task Queue that it will poll.
-1. An instance of `worker.Options`, which can be empty.
-
-Then, register the Workflow Types and the Activity Types that the Worker will be capable of executing.
-
-Lastly, call either the `Start()` or the `Run()` method on the instance of the Worker.
-Run accepts an interrupt channel as a parameter, so that the Worker can be stopped in the terminal.
-Otherwise, the `Stop()` method must be called to stop the Worker.
-*/
-
 func main() {
 	// Create a Temporal Client
 	// A Temporal Client is a heavyweight object that should be created just once per process.
@@ -64,64 +50,3 @@ func main() {
 		log.Fatalln("Unable to start Worker", err)
 	}
 }
-
-/*
-In Go, by default, the Workflow Type name is the same as the function name.
-
-To customize the Workflow Type, set the `Name` parameter with `RegisterOptions` when registering your Workflow with a Worker.
-*/
-
-/*
-To customize the Activity Type, set the `Name` parameter with `RegisterOptions` when registering your Activity with a Worker.
-*/
-
-/*
-
-:::tip
-
-If you have [`gow`](https://github.com/mitranim/gow) installed, the Worker Process automatically "reloads" when you update the Worker file:
-
-```bash
-go install github.com/mitranim/gow@latest
-gow run worker/main.go # automatically reloads when file changes
-```
-
-:::
-
-*/
-
-/* @dacx
-id: how-to-develop-a-worker-in-go
-title: How to develop a Worker in Go
-label: Develop Worker
-description: Develop an instance of a Worker by calling worker.New(), available via the go.temporal.io/sdk/worker package.
-tags:
-- go sdk
-- code sample
-- worker
-lines: 1-40, 46-55, 61-66, 78-91
-@dacx */
-
-/* @dacx
-id: how-to-customize-workflow-type-in-go
-title: How to customize Workflow Type in Go
-label: Customize Workflow Type
-description: To customize the Workflow Type set the Name parameter with RegisterOptions when registering your Workflow with a Worker.
-tags:
-- go sdk
-- code sample
-- worker
-lines: 1-12, 28, 37, 41-45, 66-72
-@dacx */
-
-/* @dacx
-id: how-to-customize-activity-type-in-go
-title: How to customize Activity Type in Go
-label: Customize Activity Type
-description: To customize the Workflow Type set the Name parameter with RegisterOptions when registering your Workflow with a Worker.
-tags:
-- go sdk
-- code sample
-- worker
-lines: 28, 37, 56-62, 66, 74-76
-@dacx */
