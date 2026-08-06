25cb09e1a645fcfaae10dd79e83f866ddfc44a5a
Evan Reynolds <evan.reynolds@temporal.io>
2026-08-06 14:38:48 -0700

    Adding documentation for Nexus Client library code generation

--- sidebars.js
+++ sidebars.js
@@ -2021,6 +2021,7 @@ module.exports = {
           },
           items: [
             'encyclopedia/nexus/nexus-services',
+            'encyclopedia/nexus/nexus-client-code-generator',
             'encyclopedia/nexus/nexus-operations',
             'encyclopedia/nexus/standalone-nexus-operation',
             'encyclopedia/nexus/nexus-endpoints',
