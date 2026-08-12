a6fe05ed7d8f0c76f47eae273c18c9d863ffcfd7
Jwahir Sundai <jwahir.sundai@temporal.io>
2026-08-12 17:46:40 -0500

    draft

--- sidebars.js
+++ sidebars.js
@@ -205,6 +205,7 @@ const developGoCategory = {
         items: [
           'develop/go/workers/run-worker-process',
           'develop/go/workers/sessions',
+          'develop/go/workers/interceptors',
           {
             type: 'category',
             label: 'Serverless Workers',
@@ -356,6 +357,7 @@ const developJavaCategory = {
         },
         items: [
           'develop/java/workers/run-worker-process',
+          'develop/java/workers/interceptors',
           {
             type: 'category',
             label: 'Serverless Workers',
